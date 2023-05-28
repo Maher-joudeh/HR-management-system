60c0c75ae108b5df9122863484b553e22903c025
Maher-joudeh <maherajoudeh@gmail.com>
2023-05-28 20:23:53 +0000

    first commit

--- js/HR.js
+++ js/HR.js
@@ -2,88 +2,96 @@
 
 /* Employee object  constructor  */
 
+
 const WyEmployee = [];
 
 function Employee(id, name, department, level, image, salary) {
-  this.id = id;
-  this.name = name;
-  this.department = department;
-  this.level = level;
-  this.image = image;
-  this.salary = 0;
-  WyEmployee.push(this);
+    this.id = id;
+    this.name = name;
+    this.department = department;
+    this.level = level;
+    this.image = image;
+    this.salary = 0;
+    WyEmployee.push(this);
 }
 
-        let Ghazi = new Employee(
-        1000,
-        "Ghazi Samer",
-        "Administration",
-        "Senior",
-        "./assets/m1.png"
-        );
+let counter = 1000;
 
-        let Lana = new Employee(
-        1001,
-        "Lana Ali",
-        "Finance",
-        "Senior",
-        "./assets/w1.png"
-        );
+function IDno() {
+    const employeeID = counter;
+    counter++;
+    return employeeID;
+}
 
-        let Tamara = new Employee(
-        1002,
-        "Tamara Ayoub",
-        "Marketing",
-        "Senior",
-        "./assets/w2.png"
-        );
+let Ghazi = new Employee(
+  IDno(),
+  "Ghazi Samer",
+  "Administration",
+  "Senior",
+  "./assets/m1.png"
+);
 
-        let Safi = new Employee(
-        1003,
-        "Safi Walid",
-        "Administration",
-        "Mid-Senior",
-        "./assets/m2.png"
-        );
+let Lana = new Employee(
+  IDno(),
+  "Lana Ali",
+  "Finance",
+  "Senior",
+  "./assets/w1.png"
+);
 
-        let Omar = new Employee(
-        1004,
-        "Omar Zaid	",
-        "Development",
-        "Senior",
-        "./assets/m3.png"
-        );
+let Tamara = new Employee(
+  IDno(),
+  "Tamara Ayoub",
+  "Marketing",
+  "Senior",
+  "./assets/w2.png"
+);
 
-        let Rana = new Employee(
-        1005,
-        "Rana Saleh",
-        "Developmen",
-        "Junior",
-        "./assets/w3.png"
-        );
+let Safi = new Employee(
+  IDno(),
+  "Safi Walid",
+  "Administration",
+  "Mid-Senior",
+  "./assets/m2.png"
+);
 
-        let Hadi = new Employee(
-        1006,
-        "Hadi Ahmad",
-        "Finance",
-        "Mid-Senior",
-        "./assets/m4.png"
-        );
+let Omar = new Employee(
+  IDno(),
+  "Omar Zaid",
+  "Development",
+  "Senior",
+  "./assets/m3.png"
+);
 
+let Rana = new Employee(
+  IDno(),
+  "Rana Saleh",
+  "Development",
+  "Junior",
+  "./assets/w3.png"
+);
+
+let Hadi = new Employee(
+  IDno(),
+  "Hadi Ahmad",
+  "Finance",
+  "Mid-Senior",
+  "./assets/m4.png"
+);
 
 Employee.prototype.salNet = function () {
-        if (this.level == "Senior") {
-            let net = Math.floor(Math.random() * (2000 - 1500)) + 1500;
-            this.salary = net * (1 - 0.075);
-        } else if (this.level == "Mid-Senior") {
-            let net = Math.floor(Math.random() * (1500 - 1000)) + 1000;
-            this.salary = net * (1 - 0.075);
-        } else if (this.level == "Junior") {
-            let net = Math.floor(Math.random() * (1000 - 500)) + 500;
-            this.salary = net * (1 - 0.075);
-        }
+    if (this.level == "Senior") {
+        let net = Math.floor(Math.random() * (2000 - 1500)) + 1500;
+        this.salary = net * (1 - 0.075);
+    } else if (this.level == "Mid-Senior") {
+        let net = Math.floor(Math.random() * (1500 - 1000)) + 1000;
+        this.salary = net * (1 - 0.075);
+    } else if (this.level == "Junior") {
+        let net = Math.floor(Math.random() * (1000 - 500)) + 500;
+        this.salary = net * (1 - 0.075);
+    }
 
-        return this.salary;
+    return this.salary;
 };
 
 function salEmp() {
@@ -91,18 +99,14 @@ function salEmp() {
         WyEmployee[i].salNet();
         WyEmployee[i].render();
     }
-
 }
 
 Employee.prototype.render = function () {
-    {
-        document.write(`<p>Employee Name:${this.name}</p>`);
-        document.write(`<p> Employee Departement:${this.department}</h3>`);
-        document.write(`<p>Employee Salary:${this.salary}</p>`);
-    }
+    document.write(`<p>Employee ID: ${this.id}</p>`);
+    document.write(`<p>Employee Name: ${this.name}</p>`);
+    document.write(`<p>Employee Department: ${this.department}</p>`);
+    document.write(`<p>Employee Salary: ${this.salary}</p>`);
+    document.write("<br>");
 };
+
 salEmp();
-
-
-
-
